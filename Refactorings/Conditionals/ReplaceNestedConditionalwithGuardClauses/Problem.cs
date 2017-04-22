f61a9f9ffe2511551ee50f2f5f5ee8340dde2acb
marcelooliveira <mclricardo@gmail.com>
2017-04-22 15:26:22 -0300

    .

--- Refactorings/Conditionals/ReplaceNestedConditionalwithGuardClauses/Problem.cs
+++ Refactorings/Conditionals/ReplaceNestedConditionalwithGuardClauses/Problem.cs
@@ -6,5 +6,58 @@ namespace Refactorings.Conditionals.ReplaceNestedConditionalwithGuardClauses
 {
     class Problema
     {
+        private bool isDead;
+        private bool isSeparated;
+        private bool isRetired;
+
+        public double GetPayAmount()
+        {
+            double result;
+
+            if (isDead)
+            {
+                result = DeadAmount();
+            }
+            else
+            {
+                if (isSeparated)
+                {
+                    result = SeparatedAmount();
+                }
+                else
+                {
+                    if (isRetired)
+                    {
+                        result = RetiredAmount();
+                    }
+                    else
+                    {
+                        result = NormalPayAmount();
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private double NormalPayAmount()
+        {
+            throw new NotImplementedException();
+        }
+
+        private double RetiredAmount()
+        {
+            throw new NotImplementedException();
+        }
+
+        private double SeparatedAmount()
+        {
+            throw new NotImplementedException();
+        }
+
+        private double DeadAmount()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Refactorings/Conditionals/ReplaceNestedConditionalwithGuardClauses/Solution.cs
+++ Refactorings/Conditionals/ReplaceNestedConditionalwithGuardClauses/Solution.cs
@@ -6,5 +6,45 @@ namespace Refactorings.Conditionals.ReplaceNestedConditionalwithGuardClauses
 {
     class Solution
     {
+        private bool isDead;
+        private bool isSeparated;
+        private bool isRetired;
+
+        public double GetPayAmount()
+        {
+            if (isDead)
+            {
+                return DeadAmount();
+            }
+            if (isSeparated)
+            {
+                return SeparatedAmount();
+            }
+            if (isRetired)
+            {
+                return RetiredAmount();
+            }
+            return NormalPayAmount();
+        }
+
+        private double NormalPayAmount()
+        {
+            throw new NotImplementedException();
+        }
+
+        private double RetiredAmount()
+        {
+            throw new NotImplementedException();
+        }
+
+        private double SeparatedAmount()
+        {
+            throw new NotImplementedException();
+        }
+
+        private double DeadAmount()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
