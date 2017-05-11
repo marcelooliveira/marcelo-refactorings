d3feffd7ee5365e4a113d8159b130293cc105d83
marcelooliveira <mclricardo@gmail.com>
2017-05-11 00:26:02 -0300

    Problem e Solution

---
+++ Refactorings/SimplifyingCalls/PreserveWholeObject/Solution.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Refactorings.SimplifyingCalls.PreserveWholeObject
+{
+    class Class1
+    {
+    }
+}
---
+++ Refactorings/SimplifyingCalls/ReplaceExceptionwithTest/Solution.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Refactorings.SimplifyingCalls.ReplaceExceptionwithTest
+{
+    class Solution
+    {
+    }
+}
