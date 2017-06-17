e44182da685a6d740a799330a480e5f590938db6
marcelooliveira <mclricardo@gmail.com>
2017-06-16 21:06:26 -0300

    RemoveControlFlag

--- Refactorings/Conditionals/RemoveControlFlag/Solution.cs
+++ Refactorings/Conditionals/RemoveControlFlag/Solution.cs
@@ -6,5 +6,41 @@ namespace Refactorings.Conditionals.RemoveControlFlag
 {
     class Solution
     {
+        public Solution()
+        {
+            IEnumerable<string> people = new List<string>();
+            CheckSecurity(people);
+        }
+
+        private void CheckSecurity(IEnumerable<string> people)
+        {
+            string foundPerson = string.Empty;
+            foundPerson = FindPerson(people);
+            var p = foundPerson;
+        }
+
+        private static string FindPerson(IEnumerable<string> people)
+        {
+            foreach (var person in people)
+            {
+                if (person.Equals("Don"))
+                {
+                    SendAlert();
+                    return person;
+                }
+                else if (person.Equals("John"))
+                {
+                    SendAlert();
+                    return person;
+                }
+            }
+
+            return string.Empty;
+        }
+
+        private static void SendAlert()
+        {
+            var blabla = 0;
+        }
     }
 }
